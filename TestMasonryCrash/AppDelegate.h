9694bcff5fdd18032f033e5a9ab6d065281cd55b
bigParis <1006599994@qq.com>
2016-05-27 16:06:42 +0800

    Initial Commit

---
+++ TestMasonryCrash/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestMasonryCrash
+//
+//  Created by yy on 16/5/27.
+//  Copyright © 2016年 BP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
